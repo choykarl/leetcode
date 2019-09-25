30103313d7449a7507b6151e3f16adecd420435b
ChoiKarl <253440030@qq.com>
2019-09-25 14:41:11 +0800

    删除链表重复节点完善

--- 数据结构与算法/数据结构与算法/leetcode/删除排序链表中的重复元素/RemoveDuplicatesFromSortedList.swift
+++ 数据结构与算法/数据结构与算法/leetcode/删除排序链表中的重复元素/RemoveDuplicatesFromSortedList.swift
@@ -9,6 +9,9 @@
 
 import Foundation
 
+/**
+ deleteDuplicates,deleteDuplicates1,deleteDuplicates2都可以处理无序的链表
+ */
 class RemoveDuplicatesFromSortedList: LeetCode {
     
     // 挨个找
@@ -80,14 +83,58 @@ class RemoveDuplicatesFromSortedList: LeetCode {
         return head
     }
     
+    // 将出现的值存到数组里,下次再出现这个值,直接过滤掉
+    func deleteDuplicates3(_ head: ListNode?) -> ListNode? {
+        if head == nil || head?.next == nil {
+            return head
+        }
+
+        var cache = [Int]()
+
+        let dumpHead = ListNode(0)
+        var moveNode = head?.next
+        var joinNode = ListNode(head!.val)
+        cache.append(head!.val)
+        dumpHead.next = joinNode
+        while let tmp = moveNode {
+            moveNode = moveNode?.next
+            if !cache.contains(tmp.val) {
+                cache.append(tmp.val)
+                joinNode.next = tmp
+                joinNode = tmp
+                tmp.next = nil
+            }
+        }
+
+        return dumpHead.next
+    }
+    
+    // 只能删除有序链表的重复节点
+    func deleteDuplicates4(_ head: ListNode?) -> ListNode? {
+        if head == nil || head?.next == nil {
+            return head
+        }
+        
+        var cur = head
+        
+        while cur != nil {
+            if cur?.val == cur?.next?.val {
+                cur?.next = cur?.next?.next
+            } else {
+                cur = cur?.next
+            }
+        }
+        
+        return head
+    }
+    
     static func execute() {
         let node1 = ListNode(1)
-        let node2 = ListNode(2)
-        let node3 = ListNode(3)
+        let node2 = ListNode(1)
+        let node3 = ListNode(2)
         let node4 = ListNode(3)
         let node5 = ListNode(4)
-        let node6 = ListNode(3)
-         // (6 - 1) * 6 / 2
+        let node6 = ListNode(4)
         node1.next = node2
         node2.next = node3
         node3.next = node4
@@ -95,7 +142,7 @@ class RemoveDuplicatesFromSortedList: LeetCode {
         node5.next = node6
         
         let ins = RemoveDuplicatesFromSortedList()
-        let res = ins.deleteDuplicates2(node1)
+        let res = ins.deleteDuplicates4(node1)
         print(res?.log() ?? "")
         
     }
