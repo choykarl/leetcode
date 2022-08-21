57297c51ff3480da874b9f42e25d1255efc252b8
ChoiKarl <253440030@qq.com>
2022-08-21 15:32:25 +0800

    增加树的遍历

--- 数据结构与算法/数据结构与算法/dataStructure/二叉树/TreeNode.swift
+++ 数据结构与算法/数据结构与算法/dataStructure/二叉树/TreeNode.swift
@@ -20,5 +20,70 @@ public class TreeNode {
     var isLeaf: Bool {
         return left == nil && right == nil
     }
+    
+    func cxbl() -> [[Int]] {
+        var result = [[Int]]()
+        var queue = [TreeNode]()
+        queue.append(self)
+        while (queue.count != 0) {
+            let count = queue.count
+            var deepList = [Int]()
+            for _ in 0 ..< count {
+                let node = queue.removeFirst()
+                deepList.append(node.val)
+                if let left = node.left {
+                    queue.append(left)
+                }
+                if let right = node.right {
+                    queue.append(right)
+                }
+            }
+            result.append(deepList)
+        }
+        
+        return result
+    }
+    
+    func qxbl() -> [Int] {
+        var result = [Int]()
+        func bl(node: TreeNode?) {
+            guard let node = node else {
+                return
+            }
+            result.append(node.val)
+            bl(node: node.left)
+            bl(node: node.right)
+        }
+        bl(node: self)
+        return result
+    }
+    
+    func zxbl() -> [Int] {
+        var result = [Int]()
+        func bl(node: TreeNode?) {
+            guard let node = node else {
+                return
+            }
+            bl(node: node.left)
+            result.append(node.val)
+            bl(node: node.right)
+        }
+        bl(node: self)
+        return result
+    }
+    
+    func hxbl() -> [Int] {
+        var result = [Int]()
+        func bl(node: TreeNode?) {
+            guard let node = node else {
+                return
+            }
+            bl(node: node.left)
+            bl(node: node.right)
+            result.append(node.val)
+        }
+        bl(node: self)
+        return result
+    }
 }
 
--- 数据结构与算法/数据结构与算法/main.swift
+++ 数据结构与算法/数据结构与算法/main.swift
@@ -78,4 +78,40 @@ import Foundation
 
 //MaximumBinaryTree.execute()
 
-CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.execute()
+//CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.execute()
+
+let n1 = TreeNode(1)
+let n2 = TreeNode(2)
+let n3 = TreeNode(3)
+let n4 = TreeNode(4)
+let n5 = TreeNode(5)
+let n6 = TreeNode(6)
+let n7 = TreeNode(7)
+let n8 = TreeNode(8)
+let n9 = TreeNode(9)
+let n10 = TreeNode(10)
+let n11 = TreeNode(11)
+let n12 = TreeNode(12)
+
+n1.left = n2
+n1.right = n3
+
+n2.left = n4
+n2.right = n5
+
+n3.right = n6
+
+n4.left = n7
+
+n6.right = n8
+
+n5.left = n9
+n5.right = n10
+
+n10.left = n11
+n10.right = n12
+
+print(n1.qxbl())
+print(n1.zxbl())
+print(n1.hxbl())
+print(n1.cxbl().flatMap({$0}))
